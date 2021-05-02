d1927564cb7cf5b95cdd97db83bb827ed9e80637
Daff <316081287@pcpuma.acatlan.unam.mx>
2021-05-02 09:47:13 -0500

    Initial deploy

--- static/main.js
+++ static/main.js
@@ -1,3 +1,7 @@
 'use strict'
 
-console.log("hola");
+var desc =  document.getElementById("description");
+
+desc.addEventListener('focus', function(){
+    desc.style.background = "rgba(0,0,0,0.2)";
+});
