7ae69f926dbf8444076e026f75db9f687796b3b5
Daff <316081287@pcpuma.acatlan.unam.mx>
2021-05-02 09:24:44 -0500

    Initial deploy

--- static/main.js
+++ static/main.js
@@ -2,8 +2,11 @@
 
 var boton =  document.querySelector(".btn");
 
-boton.addEventListener('mouseover', function(ev){
-    ev.stopPropagation();
+boton.addEventListener('mouseover', function(){
     boton.style.transform = "scale(1.05,1.05)";
+});
+
+boton.addEventListener('mouseout', function(){
+    boton.style.transform = "scale(1,1)";
 });
 
